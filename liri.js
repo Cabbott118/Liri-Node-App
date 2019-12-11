3f5d94537515e1f4a9dbe0369ec3c184d5b1e765
Caleb Abbott <54942782+Cabbott118@users.noreply.github.com>
2019-12-11 10:56:06 -0500

    Setting up Environments

---
+++ liri.js
@@ -0,0 +1,15 @@
+require("dotenv").config();
+var keys = require("./keys.js");
+var spotify = new Spotify(keys.spotify);
+
+// "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
+
+// * `concert-this`
+
+// * `spotify-this-song`
+
+// * `movie-this`
+
+// * `do-what-it-says`
+
+// Commands for liri.js (Switch Case)
